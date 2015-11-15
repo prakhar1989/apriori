389d09890f69d9122c097ee1c586e9d32ad9f889
Prakhar Srivastav <prakhar1989@gmail.com>
2015-11-15 02:28:14 -0500

    Initial commit

---
+++ load.py
@@ -0,0 +1,31 @@
+import sqlite3
+import csv
+
+def readFile(filename):
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        for (index, row) in enumerate(reader):
+            if index:
+                addInTable(row, c)
+    conn.commit()
+    conn.close()
+
+def addInTable(row, c):
+    truncated_row = filter(len, row)
+    if len(truncated_row) == 6:
+        c.execute('INSERT INTO school VALUES (?, ?, ?, ?, ?, ?)', truncated_row)
+
+def setup():
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE school
+                (dbn TEXT, peer_index REAL, overall_grade TEXT,
+                 env_grade TEXT, perf_grade TEXT, progress_grade TEXT)''')
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    setup()
+    readFile('school_report.csv')
---
+++ readme.md
@@ -0,0 +1,2 @@
+https://data.cityofnewyork.us/Education/school-report-by-zip/87qa-8iha
+https://data.cityofnewyork.us/Education/School-Progress-Reports-All-Schools-2009-10/ffnc-f3aa
