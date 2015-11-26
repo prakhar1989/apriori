9a866e0a9eafea1bdd9e0371e5aa1d27e36810cb
Prakhar Srivastav <prakhar1989@gmail.com>
2015-11-26 01:49:07 -0500

    renamed files

--- main.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-categories = {
-    'env_grade': set([x + '2' for x in "ABCDE"]),
-    'perf_grade': set([x + '3' for x in "ABCDE"]),
-    'progress_grade': set([x + '4' for x in "ABCDE"])
-}
-print categories
